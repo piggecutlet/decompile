6d232e193745aac975f0fa0bb6a0bf6cd94f3259
piggecutlet <234113501+piggecutlet@users.noreply.github.com>
2026-07-11 12:52:38 +0900

    apksigner.cmd と sign.cmd を１つにまとめる

--- scripts/sign.cmd
+++ scripts/sign.cmd
@@ -5,7 +5,7 @@ chcp 65001 > nul
 echo 【署名】開始
 
 @REM java --enable-native-access=ALL-UNNAMED -jar apksigner.jar
-call apksigner "-J-enable-native-access=ALL-UNNAMED" sign --ks %USERPROFILE%/.android/debug.keystore --ks-pass pass:android output.apk
+call %LOCALAPPDATA%/Android/Sdk/build-tools/36.1.0/apksigner.bat "-J-enable-native-access=ALL-UNNAMED" sign --ks %USERPROFILE%/.android/debug.keystore --ks-pass pass:android output.apk
 del output.apk.idsig 2>nul
 
 echo 【署名】終了
