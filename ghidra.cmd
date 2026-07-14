d349041343cbab81eb3c647c229b825ade57cfb1
piggecutlet <234113501+piggecutlet@users.noreply.github.com>
2026-07-14 09:04:14 +0900

    git ディレクトリ移動

--- ghidra.cmd
+++ ghidra.cmd
@@ -4,11 +4,11 @@ chcp 65001 > nul
 
 echo 【Ghidra】開始
 
-call %~dp0../ghidra/ghidra_12.0_PUBLIC/ghidraRun.bat %*
+call %~dp0../ghidra/ghidra_12.1.2_PUBLIC/ghidraRun.bat %*
 
 echo 【Ghidra】終了
 echo.
 
 endlocal
 
-pause
+timeout /t 3
